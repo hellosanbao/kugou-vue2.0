21df2db23018dde63d98cdbf6c52cc1257e85a12
hellosanbao <251766524@qq.com>
2017-03-19 17:33:27 +0800

    gh-pages

--- src/config.js
+++
@@ -1,9 +0,0 @@
-
-export default{
-	delegateUrl:'https://bird.ioliu.cn/v1/?url=',
-	kugouRootPath:"m.kugou.com",
-	apiSearch:'http://mobilecdn.kugou.com/api/v3/search/song?format=jsonp&pagesize=30&showtype=1',
-	apiSongs:'http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo',
-	apilyrc:'http://cs003.m2828.com/apis/getLrc.php'
-}
-
